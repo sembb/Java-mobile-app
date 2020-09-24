18c8dcbfabed141e69eaf5825ee29c344dd44194
sembb <sembarelds37@gmail.com>
2020-09-24 10:38:47 +0200

    Ik heb een stukje code van internet gebruikt op te proberen een basis inlogsysteem te maken, een van de functies is deprecated dus ik zal het waarschijnlijk opnieuw moeten bedenken.

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editEmail, editPassword, editName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +34,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginBtn = findViewById(R.id.btnSignIn);
+        editName = (EditText)findViewById(R.id.editTextTextPersonName);
+        editPassword = (EditText)findViewById(R.id.editTextTextPassword);
+
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 TextView testtekst = findViewById(R.id.testtekst);
                 testtekst.setText("Nu moet je inloggen");
+                AttemptLogin attemptLogin= new AttemptLogin();
+                attemptLogin.execute(editName.getText().toString(),editPassword.getText().toString(),"");
             }
         });
+    }
+
+    private class AttemptLogin extends AsyncTask<String, String, JSONObject> {
+
+        @Override
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+        }
+
+        @Override
+
+        protected JSONObject doInBackground(String... args) {
+
+
+            String email = args[2];
+            String password = args[1];
+            String name = args[0];
+
+            ContentValues params=new ContentValues();
+            params.put("username", name);
+            params.put("password", password);
+            if (email.length() > 0)
+                params.put("email", email);
+
+            JSONObject json = JsonParser.makeHttpRequest(URL, "POST", params);
+
+
+            return json;
+
+        }
+
+        protected void onPostExecute(JSONObject result) {
+
+            // dismiss the dialog once product deleted
+            //Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+
+            try {
+                if (result != null) {
+                    Toast.makeText(getApplicationContext(), result.getString("message"), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unable to retrieve any data from server", Toast.LENGTH_LONG).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
